3f5b7f3aa1d923af5aaecb2501e240d1aedd8c11
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-11-02 08:01:40 +0700

    Implement list of data.

--- app/src/main/java/com/taufik/jetpackcompose/ui/compose/basic/composebasic/ComposeBasicActivity.kt
+++ app/src/main/java/com/taufik/jetpackcompose/ui/compose/basic/composebasic/ComposeBasicActivity.kt
@@ -3,36 +3,73 @@ package com.taufik.jetpackcompose.ui.compose.basic.composebasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ExpandLess
+import androidx.compose.material.icons.outlined.ExpandMore
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.taufik.jetpackcompose.R
 import com.taufik.jetpackcompose.ui.theme.JetpackComposeTheme
+
+private val sampleName = listOf(
+    "Andre",
+    "Desta",
+    "Parto",
+    "Wendy",
+    "Komeng",
+    "Raffi Ahmad",
+    "Andhika Pratama",
+    "Vincent Ryan Rompies"
+)
 
 class ComposeBasicActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                HelloJetpackComposeApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HelloJetpackComposeApp() {
+    // A surface container using the 'background' color from the theme
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        GreetingList(sampleName)
+    }
+}
+
+@Composable
+fun GreetingList(names: List<String>) {
+    if (names.isNotEmpty()) {
+       LazyColumn {
+           items(names) { name ->
+               Greeting(name)
+           }
+       }
+    } else {
+        Text(text = "No people to greet :(")
+    }
 }
 
 @Preview(
@@ -43,8 +80,55 @@ fun Greeting(name: String) {
     showBackground = true
 )
 @Composable
+fun HelloJetpackComposeAppPreview() {
+    JetpackComposeTheme {
+        HelloJetpackComposeApp()
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    var isExpanded by remember { mutableStateOf(false) }
+    val animatedSizeDp by animateDpAsState(
+        targetValue = if (isExpanded) 120.dp else 80.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+    Row(
+        modifier = Modifier.padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.jetpack_compose),
+            contentDescription = "Foreground Image",
+            modifier = Modifier.size(animatedSizeDp)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(
+                text = "Hello $name!",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+            )
+            Text(text = "Welcome to Dicoding!")
+        }
+        IconButton(onClick = { isExpanded = !isExpanded }) {
+            Icon(
+                imageVector = if (isExpanded) Icons.Outlined.ExpandLess else Icons.Outlined.ExpandMore,
+                contentDescription = if (isExpanded) "Show Less" else "Show More"
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
 fun DefaultPreview() {
     JetpackComposeTheme {
-        Greeting("Android")
+        Greeting("Jetpack Compose")
     }
 }
