5a0b0562feb2509c7c2bc3f2a1f2c5ec808e9b50
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-11-16 06:56:21 +0700

    Add menu category and section text.

--- app/src/main/java/com/taufik/jetpackcompose/ui/compose/layout/composebanner/ui/JetCoffeeActivity.kt
+++ app/src/main/java/com/taufik/jetpackcompose/ui/compose/layout/composebanner/ui/JetCoffeeActivity.kt
@@ -4,18 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.taufik.jetpackcompose.R
+import com.taufik.jetpackcompose.ui.compose.layout.composebanner.model.dummyCategory
+import com.taufik.jetpackcompose.ui.compose.layout.composebanner.ui.components.CategoryItem
 import com.taufik.jetpackcompose.ui.compose.layout.composebanner.ui.components.SearchBar
+import com.taufik.jetpackcompose.ui.compose.layout.composebanner.ui.components.SectionText
 import com.taufik.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class JetCoffeeActivity : ComponentActivity() {
@@ -43,9 +47,34 @@ fun Banner(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun JetCoffeeApp() {
-    Column {
+fun CategoryRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier.padding(16.dp)
+    ) {
+        items(dummyCategory, key = { it.textCategory }) { category ->
+            CategoryItem(category)
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun CategoryRowPreview() {
+    JetpackComposeTheme {
+        CategoryRow()
+    }
+}
+
+@Composable
+fun JetCoffeeApp(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
         Banner()
+        SectionText(title = stringResource(R.string.section_category))
+        CategoryRow()
     }
 }
 
