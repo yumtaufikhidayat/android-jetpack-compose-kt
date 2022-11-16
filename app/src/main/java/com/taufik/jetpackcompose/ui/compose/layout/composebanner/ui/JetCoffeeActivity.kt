65d6cbb9dad06f607217d047f60acc51744416f5
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-11-17 06:28:00 +0700

    Add menu section.

--- app/src/main/java/com/taufik/jetpackcompose/ui/compose/layout/composebanner/ui/JetCoffeeActivity.kt
+++ app/src/main/java/com/taufik/jetpackcompose/ui/compose/layout/composebanner/ui/JetCoffeeActivity.kt
@@ -16,8 +16,12 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.taufik.jetpackcompose.R
+import com.taufik.jetpackcompose.ui.compose.layout.composebanner.model.Menu
+import com.taufik.jetpackcompose.ui.compose.layout.composebanner.model.dummyBestSellerMenu
 import com.taufik.jetpackcompose.ui.compose.layout.composebanner.model.dummyCategory
+import com.taufik.jetpackcompose.ui.compose.layout.composebanner.model.dummyMenu
 import com.taufik.jetpackcompose.ui.compose.layout.composebanner.ui.components.CategoryItem
+import com.taufik.jetpackcompose.ui.compose.layout.composebanner.ui.components.MenuItem
 import com.taufik.jetpackcompose.ui.compose.layout.composebanner.ui.components.SearchBar
 import com.taufik.jetpackcompose.ui.compose.layout.composebanner.ui.components.SectionText
 import com.taufik.jetpackcompose.ui.theme.JetpackComposeTheme
@@ -62,6 +66,22 @@ fun CategoryRow(
 }
 
 @Composable
+fun MenuRow(
+    listMenu: List<Menu>,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(listMenu, key = { it.title }) { menu ->
+            MenuItem(menu)
+        }
+    }
+}
+
+@Composable
 @Preview(showBackground = true)
 fun CategoryRowPreview() {
     JetpackComposeTheme {
@@ -75,6 +95,10 @@ fun JetCoffeeApp(modifier: Modifier = Modifier) {
         Banner()
         SectionText(title = stringResource(R.string.section_category))
         CategoryRow()
+        SectionText(title = stringResource(R.string.section_favorite_menu))
+        MenuRow(listMenu = dummyMenu)
+        SectionText(title = stringResource(R.string.section_best_seller_menu))
+        MenuRow(listMenu = dummyBestSellerMenu)
     }
 }
 
