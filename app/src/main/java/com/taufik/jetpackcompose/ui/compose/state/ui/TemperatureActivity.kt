37f9f00711fe13f52bcc092687f7aa8fab30cafb
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-11-19 06:32:21 +0700

    Add stateful converter.

---
+++ app/src/main/java/com/taufik/jetpackcompose/ui/compose/state/ui/TemperatureActivity.kt
@@ -0,0 +1,86 @@
+package com.taufik.jetpackcompose.ui.compose.state.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.taufik.jetpackcompose.R
+import com.taufik.jetpackcompose.ui.compose.state.ui.ui.theme.JetpackComposeTheme
+
+class TemperatureActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeTheme {
+                TemperatureApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun TemperatureApp() {
+    // A surface container using the 'background' color from the theme
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        StatefulTemperatureInput()
+    }
+}
+
+@Composable
+fun StatefulTemperatureInput(
+    modifier: Modifier = Modifier
+) {
+    var input by remember { mutableStateOf("") }
+    var output by remember { mutableStateOf("") }
+    Column(modifier.padding(16.dp)) {
+        Text(
+            text = stringResource(R.string.stateful_converter),
+            style = MaterialTheme.typography.h5
+        )
+        OutlinedTextField(
+            value = input,
+            label = { Text(text = stringResource(id = R.string.enter_celsius)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = { newInput ->
+                input = newInput
+                output = convertToFahrenheit(newInput)
+            }
+        )
+        Text(text = stringResource(id = R.string.temperature_fahrenheit, output))
+    }
+}
+
+private fun convertToFahrenheit(celsius: String): String {
+    return celsius.toDoubleOrNull()?.let {
+        (it * 9 / 5) + 32
+    }.toString()
+}
+
+@Preview(
+    showBackground = true,
+    device = Devices.PIXEL_3A,
+    showSystemUi = true
+)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeTheme {
+        TemperatureApp()
+    }
+}
