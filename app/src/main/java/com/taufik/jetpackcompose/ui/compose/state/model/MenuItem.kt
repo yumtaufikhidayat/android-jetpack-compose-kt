726379fdb4252f4342b6f81a8d0cc69582ea0080
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-11-21 06:23:59 +0700

    Add navigation drawer and snackbar.

---
+++ app/src/main/java/com/taufik/jetpackcompose/ui/compose/state/model/MenuItem.kt
@@ -0,0 +1,8 @@
+package com.taufik.jetpackcompose.ui.compose.state.model
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class MenuItem(
+    val title: String,
+    val icon: ImageVector
+)
