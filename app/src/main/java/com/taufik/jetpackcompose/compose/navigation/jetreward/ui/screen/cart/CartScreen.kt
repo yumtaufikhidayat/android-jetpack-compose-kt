50424d2803934d66f6544e56e60b65de3b781a66
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-11-27 10:35:04 +0700

    Update cart screen.

--- app/src/main/java/com/taufik/jetpackcompose/compose/navigation/jetreward/ui/screen/cart/CartScreen.kt
+++ app/src/main/java/com/taufik/jetpackcompose/compose/navigation/jetreward/ui/screen/cart/CartScreen.kt
@@ -1,16 +1,20 @@
 package com.taufik.jetpackcompose.compose.navigation.jetreward.ui.screen.cart
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.taufik.jetpackcompose.R
 
 @Composable
 fun CartScreen(modifier: Modifier = Modifier) {
-    Box(modifier = modifier.fillMaxWidth()) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
         Text(text = stringResource(id = R.string.menu_cart))
     }
 }
