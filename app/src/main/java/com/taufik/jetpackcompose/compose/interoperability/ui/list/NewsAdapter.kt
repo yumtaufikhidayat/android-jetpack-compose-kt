3e1e381f6063453c9d8c38121dee4c3d1304ccd9
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-12-04 10:29:29 +0700

    Implement interoperability compose view on xml.

--- app/src/main/java/com/taufik/jetpackcompose/compose/interoperability/ui/list/NewsAdapter.kt
+++ app/src/main/java/com/taufik/jetpackcompose/compose/interoperability/ui/list/NewsAdapter.kt
@@ -3,14 +3,27 @@ package com.taufik.jetpackcompose.compose.interoperability.ui.list
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.ListAdapter
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
-import com.taufik.jetpackcompose.R
+import coil.compose.AsyncImage
 import com.taufik.jetpackcompose.compose.interoperability.data.local.entity.NewsEntity
 import com.taufik.jetpackcompose.databinding.ItemNewsBinding
+import androidx.compose.ui.tooling.preview.Preview
 
 class NewsAdapter(private val onItemClick: (NewsEntity) -> Unit): ListAdapter<NewsEntity, NewsAdapter.MyViewHolder>(
     DIFF_CALLBACK
@@ -32,16 +45,15 @@ class NewsAdapter(private val onItemClick: (NewsEntity) -> Unit): ListAdapter<Ne
         binding.root
     ) {
         fun bind(news: NewsEntity) {
-            binding.tvItemTitle.text = news.title
-            binding.tvItemPublishedDate.text = news.publishedAt
-            Glide.with(itemView.context)
-                .load(news.urlToImage)
-                .apply(
-                    RequestOptions.placeholderOf(R.drawable.ic_loading).error(R.drawable.ic_error)
-                )
-                .into(binding.imgPoster)
-            itemView.setOnClickListener {
-                onItemClick(news)
+            binding.composeView.setContent {
+                MaterialTheme {
+                    NewsItem(
+                        news.urlToImage,
+                        news.title,
+                        news.publishedAt,
+                        onItemClick = { onItemClick(news) }
+                    )
+                }
             }
         }
     }
@@ -58,5 +70,58 @@ class NewsAdapter(private val onItemClick: (NewsEntity) -> Unit): ListAdapter<Ne
                     return oldUser == newUser
                 }
             }
+    }
+}
+
+@Composable
+fun NewsItem(
+    urlToImage: String?,
+    title: String,
+    publishedAt: String,
+    onItemClick: () -> Unit
+) {
+    ConstraintLayout(modifier = Modifier.clickable { onItemClick() }) {
+        val (posterImage, titleText, publishedDateText) = createRefs()
+
+        AsyncImage(
+            model = urlToImage,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(120.dp)
+                .constrainAs(posterImage) {
+                    top.linkTo(parent.top)
+                }
+        )
+        Text(
+            text = title,
+            overflow = TextOverflow.Ellipsis,
+            textAlign = TextAlign.Start,
+            maxLines = 3,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.constrainAs(titleText) {
+                start.linkTo(posterImage.end, margin = 16.dp)
+                top.linkTo(parent.top, margin = 16.dp)
+                end.linkTo(parent.end, margin = 16.dp)
+                width = Dimension.fillToConstraints
+            }
+        )
+        Text(
+            text = publishedAt,
+            fontSize = 14.sp,
+            modifier = Modifier.constrainAs(publishedDateText) {
+                top.linkTo(titleText.bottom, margin = 8.dp)
+                start.linkTo(titleText.start)
+            }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun News() {
+    MaterialTheme {
+        NewsItem("", "New News", "2022-02-22") {}
     }
 }
